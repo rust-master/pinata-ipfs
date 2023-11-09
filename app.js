bb278e40c1cc583f873c47867c2fdbaea2b81ccb
Zaryab <zaryab.rafique@invozone.com>
2023-11-09 15:15:31 +0500

    formatting

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 3000; // Set your desired port number
@@ -8,17 +8,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Import the pinFileToIPFS function from your module
-const { pinFileToIPFS } = require('./pinata'); 
+const { pinFileToIPFS } = require("./pinata");
 
 // Define a route to handle file uploads and pinning to IPFS
-app.post('/upload', async (req, res) => {
+app.post("/upload", async (req, res) => {
   try {
     // Retrieve file and data from the request
     const { filePath, data } = req.body;
 
-    console.log("filePath:", filePath)
-    console.log("Data:", data)
-  
+    console.log("filePath:", filePath);
+    console.log("Data:", data);
 
     // Call the pinFileToIPFS function from your module
     const result = await pinFileToIPFS(filePath, data);
@@ -26,11 +25,16 @@ app.post('/upload', async (req, res) => {
     if (result) {
       res.status(200).json({ success: true, result });
     } else {
-      res.status(500).json({ success: false, message: 'Failed to upload and pin the file.' });
+      res
+        .status(500)
+        .json({
+          success: false,
+          message: "Failed to upload and pin the file.",
+        });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, message: 'Internal server error' });
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
 });
 
